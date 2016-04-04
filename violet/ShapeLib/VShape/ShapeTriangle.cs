f262b9f6b3dee6e37216afba745986ecab2c422c
so910327 <so910327@gmail.com>
2016-04-04 15:30:40 +0800

    triangle

--- violet/ShapeLib/VShape/ShapeTriangle.cs
+++ violet/ShapeLib/VShape/ShapeTriangle.cs
@@ -49,12 +49,17 @@ namespace ShapeLib.VShape
                 myTri.StrokeThickness = data.state.strokeT;
 
                 myTri.Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(data.state.colorR, data.state.colorG, data.state.colorB));
-        
 
-                myTri.Width = Math.Abs(data.controlBtn4.X - data.controlBtn1.X);
-                myTri.Height = Math.Abs(data.controlBtn4.Y - data.controlBtn1.Y);
+
+                //myTri.Width = Math.Abs(data.controlBtn4.X - data.controlBtn1.X);
+                //myTri.Height = Math.Abs(data.controlBtn4.Y - data.controlBtn1.Y);
+                
+                PointCollection Points = new PointCollection();
+                Points.Insert(0,data.controlBtn1);
+                Points.Insert(1, data.controlBtn2);
+                Points.Add(data.controlBtn4);
+                myTri.Points = new PointCollection(Points);
                 myTri.Margin = new Thickness(data.controlBtn1.X, data.controlBtn1.Y, 0, 0);
-
                 myTri.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
                 myTri.VerticalAlignment = VerticalAlignment.Center;
                 myTri.StrokeThickness = shapeLib.Data.strokeT;
@@ -69,10 +74,15 @@ namespace ShapeLib.VShape
             else
             {
                 Polygon myTri = (Polygon)gv.baseShape[0];// =(Line) currPath.getDrawShape();
-                myTri.Width = Math.Abs(data.controlBtn4.X - data.controlBtn1.X);
-                myTri.Height = Math.Abs(data.controlBtn4.Y - data.controlBtn1.Y);
+                //myTri.Width = Math.Abs(data.controlBtn4.X - data.controlBtn1.X);
+                //myTri.Height = Math.Abs(data.controlBtn4.Y - data.controlBtn1.Y);
+                PointCollection Points = new PointCollection();
+                Points.Insert(0, data.controlBtn1);
+                Points.Insert(1, data.controlBtn2);
+                Points.Add(data.controlBtn4);
+                myTri.Points = new PointCollection(Points);
                 myTri.Margin = new Thickness(data.controlBtn1.X, data.controlBtn1.Y, 0, 0);
-
+                
             }
 
 
