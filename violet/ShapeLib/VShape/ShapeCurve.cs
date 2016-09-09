1a13a5d9bbd72379efa9d9295f3eeee944575f92
so910327 <so910327@gmail.com>
2016-09-09 20:58:07 +0800

    curve

--- violet/ShapeLib/VShape/ShapeCurve.cs
+++ violet/ShapeLib/VShape/ShapeCurve.cs
@@ -36,25 +36,67 @@ namespace ShapeLib.VShape
 
 
 
-
+        Point[] myarr = new Point[4];
         public override void DrawShape(gView gv, gPath data, Boolean bfirst)
         {
-         
+            //Point[] myarr = { data.controlBtn4, data.controlBtn1, data.controlBtn2, data.controlBtn3 };
+            //Point a = data.controlBtn1;
+            //Point b = data.controlBtn2;
+            //Point c = data.controlBtn3;
+            //Point d = data.controlBtn4;
+                   
             
             if (bfirst)
             {
                     shapeLib.Data.Status = "rest";
                     shapeLib.Data.bfirst = false;
                     BezierSegment  bezier = new BezierSegment();
-                    bezier.Point3 = data.controlBtn3;
                     PathFigure figure = new PathFigure();
-                    figure.StartPoint = data.controlBtn1;
-                    bezier.Point1 = figure.StartPoint;
-                    bezier.Point2 = bezier.Point3;
+                    if (myarr[0] == myarr[1] && myarr[1] == myarr[2] && myarr[2] == myarr[3])
+                    {
+
+                        myarr[0] = data.controlBtn1;
+                        myarr[1] = data.controlBtn2;
+                        myarr[2] = data.controlBtn3;
+                        myarr[3] = data.controlBtn4;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if ((3 - i) > 0)
+                            {
+                                myarr[2 - i] = myarr[3 - i];
+                            }
+                            else
+                                myarr[i] = data.controlBtn1;
+
+                        }
+                        figure.StartPoint = myarr[0];
+                        bezier.Point1 = data.controlBtn2;
+                        bezier.Point2 = data.controlBtn3;
+                        bezier.Point3 = myarr[3];
+                        
+                        //bezier.Point3 = myarr[2];
+                        //figure.StartPoint = myarr[0];
+                        //bezier.Point1 = figure.StartPoint;
+                        //bezier.Point2 = bezier.Point3;
+                    }
+                    
+                    //bezier.Point3 = data.controlBtn3;
+                    //figure.StartPoint = data.controlBtn1;
+                    //bezier.Point1 = figure.StartPoint;
+                    //bezier.Point2 = bezier.Point3;
+
+
+
+                    
+                    
                     figure.Segments.Add(bezier);
                     PathGeometry geometry = new PathGeometry();
                     geometry.Figures.Add(figure);
                     Path myPath = new System.Windows.Shapes.Path();
+                    
                     SolidColorBrush mySolidColorBrush = new SolidColorBrush();
                     // Describes the brush's color using RGB values. 
                     // Each value has a range of 0-255.
@@ -72,50 +114,85 @@ namespace ShapeLib.VShape
             {
                 Path myPath = (Path)gv.baseShape[0];// =(Line) currPath.getDrawShape();
                 PathGeometry geometry = (PathGeometry)myPath.Data;
-                geometry.Figures[0].StartPoint = data.controlBtn1;
+                
+                
+               
                 BezierSegment bs = (BezierSegment)geometry.Figures[0].Segments[0];
+                //if (myarr[0] == myarr[1] && myarr[1] == myarr[2] && myarr[2] == myarr[3])
+                //{
+
+                //    myarr[0] = data.controlBtn1;
+                //    myarr[1] = data.controlBtn2;
+                //    myarr[2] = data.controlBtn3;
+                //    myarr[3] = data.controlBtn4;
+
+
+                //}
+                //else
+                //{
+                //for (int i = 0; i < 4; i++)
+                //{
+                //    if ((3 - i) > 0)
+                //    {
+                //        myarr[2 - i] = myarr[3 - i];
+                //    }
+                //    else
+                //        myarr[i] = data.controlBtn1;
+
+                //}
+
+
+                //}
+                geometry.Figures[0].StartPoint = myarr[0];
                 bs.Point1 = data.controlBtn2;
                 bs.Point2 = data.controlBtn3;
-                bs.Point3 = data.controlBtn4;
+                bs.Point3 = myarr[3];                 
+                //bs.Point1 = data.controlBtn2;
+                //bs.Point2 = data.controlBtn3;
+                //bs.Point3 = data.controlBtn4;
+                
+               
+                
+                
             }
         }
 
-        public override void DisplayControlPoints(gView gv, gPath data)
-        {
-            if (gv.controlShape.Count == 0)
-            {
-                BezierSegment bezier = new BezierSegment();
-                bezier.Point3 = data.controlBtn3;
-                PathFigure figure = new PathFigure();
-                figure.StartPoint = data.controlBtn1;
-                bezier.Point1 = figure.StartPoint;
-                bezier.Point2 = bezier.Point3;
-                figure.Segments.Add(bezier);
-                PathGeometry geometry = new PathGeometry();
-                geometry.Figures.Add(figure);
-                Path myPath = new System.Windows.Shapes.Path();
-                myPath.Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 0, 255));    
-                myPath.StrokeThickness = data.state.strokeT;
-               /* myPath.MouseLeftButtonDown += data.myLine_MouseLeftButtonDown;
-                myPath.MouseEnter += data.myLine_MouseEnter;
-                myPath.MouseLeave += data.myLine_MouseLeave;  */
-                myPath.Data = geometry;
-                shapeLib.Data.mygrid.Children.Add(myPath);
-                gv.controlShape.Add(myPath);
+        //public override void DisplayControlPoints(gView gv, gPath data)
+        //{
+        //    if (gv.controlShape.Count == 0)
+        //    {
+        //        BezierSegment bezier = new BezierSegment();
+        //        bezier.Point3 = data.controlBtn3;
+        //        PathFigure figure = new PathFigure();
+        //        //figure.StartPoint = data.controlBtn1;
+        //        //bezier.Point1 = figure.StartPoint;
+        //        //bezier.Point2 = bezier.Point3;
+        //        figure.Segments.Add(bezier);
+        //        PathGeometry geometry = new PathGeometry();
+        //        geometry.Figures.Add(figure);
+        //        Path myPath = new System.Windows.Shapes.Path();
+        //        myPath.Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 0, 255));    
+        //        myPath.StrokeThickness = data.state.strokeT;
+        //       /* myPath.MouseLeftButtonDown += data.myLine_MouseLeftButtonDown;
+        //        myPath.MouseEnter += data.myLine_MouseEnter;
+        //        myPath.MouseLeave += data.myLine_MouseLeave;  */
+        //        myPath.Data = geometry;
+        //        shapeLib.Data.mygrid.Children.Add(myPath);
+        //        gv.controlShape.Add(myPath);
 
-            }
+        //    }
 
-            else
-            {
-                Path myPath = (Path)gv.controlShape[0];// =(Line) currPath.getDrawShape();
-                PathGeometry geometry = (PathGeometry)myPath.Data;
-                geometry.Figures[0].StartPoint = data.controlBtn1;
-                BezierSegment bs = (BezierSegment)geometry.Figures[0].Segments[0];
-                bs.Point1 = data.controlBtn2;
-                bs.Point2 = data.controlBtn3;
-                bs.Point3 = data.controlBtn4;
-            }
+        //    else
+        //    {
+        //        Path myPath = (Path)gv.controlShape[0];// =(Line) currPath.getDrawShape();
+        //        PathGeometry geometry = (PathGeometry)myPath.Data;
+        //        geometry.Figures[0].StartPoint = data.controlBtn1;
+        //        BezierSegment bs = (BezierSegment)geometry.Figures[0].Segments[0];
+        //        bs.Point1 = data.controlBtn2;
+        //        bs.Point2 = data.controlBtn3;
+        //        bs.Point3 = data.controlBtn4;
+        //    }
 
-        }
+        //}
     }
 }
